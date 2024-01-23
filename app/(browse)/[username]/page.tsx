dcddcfb426dcb62c6733d7c61d30b849fcdaadd8
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2024-01-23 19:23:49 +0530

    dashboard created

---
+++ app/(browse)/[username]/page.tsx
@@ -0,0 +1,44 @@
+import { notFound } from "next/navigation";
+
+import { getUserByUsername } from "@/lib/user-service";
+import { isFollowingUser } from "@/lib/follow-service";
+import { isBlockedByUser } from "@/lib/block-service";
+// import { StreamPlayer } from "@/components/stream-player";
+
+interface UserPageProps {
+    params: {
+        username: string;
+    };
+};
+
+const UserPage = async ({
+    params
+}: UserPageProps) => {
+    const user = await getUserByUsername(params.username);
+
+    if (!user) {
+        console.log(params);
+        console.log(user)
+        notFound();
+    }
+
+    const isFollowing = await isFollowingUser(user.id);
+    const isBlocked = await isBlockedByUser(user.id);
+
+    if (isBlocked) {
+        notFound();
+    }
+
+    return (
+        // <StreamPlayer
+        //   user={user}
+        //   stream={user.stream}
+        //   isFollowing={isFollowing}
+        // />
+        <>
+            {user.username}
+        </>
+    );
+}
+
+export default UserPage;
